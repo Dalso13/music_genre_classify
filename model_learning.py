6567d11995abf999674c90f74887241e769ddbf5
Dalso <flskwhdhk@gmail.com>
2024-12-22 21:53:58 +0900

    refactor : 파일 이름 수정

--- model_learning.py
+++ model_learning.py
@@ -16,7 +16,7 @@ y = dataset.iloc[:, -1].astype(np.int64)
 
 label = len(np.unique(y))
 label_str = ["Electronic", "Experimental", "Folk", "Hip-Hop", "Instrumental", "International", "Pop", "Rock"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 cnn = Sequential([
     Input(shape=X[0].shape),
@@ -26,7 +26,7 @@ cnn = Sequential([
     MaxPooling2D(),
     Flatten(),
     Dropout(0.5),
-    Dense(256, activation='relu'),
+    Dense(128, activation='relu'),
     Dense(8, activation='softmax')
 ])
 
@@ -34,18 +34,18 @@ cnn.compile(loss=sparse_categorical_crossentropy,
             optimizer='adam',
             metrics=['acc'])
 
-result = cnn.fit(X_train, y_train,
+result = cnn.fit(X, y,
                  validation_split=0.2,
                  batch_size=64,
-                 epochs=30)
+                 epochs=10)
 
-print(cnn.evaluate(X_test, y_test))
+# print(cnn.evaluate(X_test, y_test))
+#
+# pred_proba = cnn.predict(X_test)
+#
+# pred_label = np.argmax(pred_proba, axis=1)
+#
+# incor_ind = np.argwhere(y_test != pred_label)
+# print(incor_ind.shape[0]/y_test.shape[0])
 
-pred_proba = cnn.predict(X_test)
-
-pred_label = np.argmax(pred_proba, axis=1)
-
-incor_ind = np.argwhere(y_test != pred_label)
-print(incor_ind.shape[0]/y_test.shape[0])
-
-cnn.save('music_genre_classify1.h5')
+cnn.save('music_genre_classify.h5')
